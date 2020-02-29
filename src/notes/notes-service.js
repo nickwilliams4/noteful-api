e67a8ec6347c72016b4ea490429763f8137bc64b
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2020-02-29 11:32:23 -0700

    fix typo insertNote

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -3,7 +3,7 @@ const NotesService = {
     return knex.select('*').from('notes')
   },
 
-  insertNotes(knex, newNote) {
+  insertNote(knex, newNote) {
     return knex
       .insert(newNote)
       .into('notes')
