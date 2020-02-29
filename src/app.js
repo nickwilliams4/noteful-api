a5b162947abc3c9bfd94132fcaa6fff01aa24a73
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2020-02-29 11:16:39 -0700

    testing note add

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ const notesRouter = require('./notes/notes-router')
 
 const app = express()
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
 app.use(helmet())
 
